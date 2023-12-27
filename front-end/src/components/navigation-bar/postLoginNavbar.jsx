0f6b91edf77bef8e81ce70a4582a9c9dead444bb
Pattadon <pattadon2546pn@gmail.com>
2023-12-27 23:43:38 +0700

    Update fonts and add dependencies

--- front-end/src/components/navigation-bar/postLoginNavbar.jsx
+++ front-end/src/components/navigation-bar/postLoginNavbar.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 export function PostLoginNavbar(){
+    
     return (
-    <div className="block w-screen h-1/5 bg-black m-0">
-        <p>Hello World!</p>
-    </div>
+        <div className="flex flex-row gap-5 justify-start p-5  items-center h-28 bg-black">
+            <div><a href="" className="text-white">HOME</a></div>
+            <div><a href="" className="text-white">ROOM TYPE</a></div>
+            <div><a href="" className="text-white">ABOUT</a></div>
+            <div className="ml-auto mr-auto"><p className="text-white mx-96 text-5xl font-playfair">Midnight & Haven</p></div>
+        </div>
     );
 }
