0bdff2d2b3a0094d4519c67621d38e35f2c77df8
Pattadon <pattadon2546pn@gmail.com>
2023-12-28 11:34:36 +0700

    Update background color and add menu selection functionality

--- front-end/src/components/navigation-bar/postLoginNavbar.jsx
+++ front-end/src/components/navigation-bar/postLoginNavbar.jsx
@@ -1,35 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoPersonCircleOutline } from "react-icons/io5";
 import { CiSearch } from "react-icons/ci";
 
 export function PostLoginNavbar() {
+  const [selectedMenu, setSelectedMenu] = useState("HOME");
+
+  const handleMenuClick = (menu) => {
+    setSelectedMenu(menu);
+  };
+
   return (
-    <div className="flex flex-row  gap-5 justify-start p-5  items-center h-28 bg-black">
-      <div>
-        <a href="" className="text-white font-inter">
+    <div className="flex flex-row gap-5 justify-start p-5 items-center h-28 bg-black border-b-2 border-yellow-400">
+      <div className={selectedMenu === "HOME" ? "border-b-2" : ""}>
+        <a href="" className="text-white font-inter" onClick={() => handleMenuClick("HOME")}>
           HOME
         </a>
       </div>
-      <div>
-        <a href="" className="text-white font-inter">
+      <div className={selectedMenu === "ROOM TYPE" ? "border-b-2" : ""}>
+        <a href="" className="text-white font-inter" onClick={() => handleMenuClick("ROOM TYPE")}>
           ROOM TYPE
         </a>
       </div>
-      <div>
-        <a href="" className="text-white font-inter">
+      <div className={selectedMenu === "ABOUT" ? "border-b-2" : ""}>
+        <a href="" className="text-white font-inter" onClick={() => handleMenuClick("ABOUT")}>
           ABOUT
         </a>
       </div>
       <div className="ml-16">
-        <p className="text-white mx-96 text-5xl font-playfair">
-          Midnight & Haven
-        </p>
+        <p className="text-white mx-96 text-5xl font-playfair">Midnight & Haven</p>
       </div>
       <div className="mr-18">
         <CiSearch color="white" size={40} />
       </div>
       <div className="mr-6">
-      <IoPersonCircleOutline color="white" size={40} />
+        <IoPersonCircleOutline color="white" size={40} />
       </div>
     </div>
   );
