188efdd054de94a32406e30a5dcc6b90632471c5
Pattadon <pattadon2546pn@gmail.com>
2023-12-28 12:34:33 +0700

    completed postLoginNavbar interface.

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
-import './App.css'
-import {PostLoginNavbar} from "./components/navigation-bar/postLoginNavbar.jsx"
+import React from "react";
+import "./App.css";
+import { Routes } from "react-router-dom";
+import { PostLoginNavbar } from "./components/navigation-bar/postLoginNavbar.jsx";
 
 function App() {
-
+  const postLoginPages = () => {
+    return (
+      <Routes>
+        <Route path="/"></Route>
+      </Routes>
+    );
+  }
   return (
-    <div className="App">
-      <PostLoginNavbar  />
-    </div>
-  )
+      <div className="App">
+        <PostLoginNavbar />
+      </div>
+  );
 }
 
-export default App
+export default App;
--- front-end/src/components/navigation-bar/postLoginNavbar.jsx
+++ front-end/src/components/navigation-bar/postLoginNavbar.jsx
@@ -1,37 +1,77 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { IoPersonCircleOutline } from "react-icons/io5";
 import { CiSearch } from "react-icons/ci";
 
 export function PostLoginNavbar() {
   const [selectedMenu, setSelectedMenu] = useState("HOME");
+  const [isVisible, setIsVisible] = useState(false);
 
+  // handle which menu is selected
   const handleMenuClick = (menu) => {
     setSelectedMenu(menu);
   };
 
+  // handle whether the search bar is visible or not
+  const handleSearchButtonClick = () => {
+    setIsVisible(!isVisible);
+  };
+
   return (
-    <div className="flex flex-row gap-5 justify-start p-5 items-center h-28 bg-black border-b-2 border-yellow-400">
-      <div className={selectedMenu === "HOME" ? "border-b-2" : ""}>
-        <a href="" className="text-white font-inter" onClick={() => handleMenuClick("HOME")}>
+    <div className="flex flex-row gap-5 justify-start p-5 items-center h-28 bg-black border-b-2 border-gray-400">
+      <div
+        className={
+          selectedMenu === "HOME" ? "border-b-2 border-yellow-600" : ""
+        }
+      >
+        <Link
+          to="/"
+          className="text-white font-inter"
+          onClick={() => handleMenuClick("HOME")}
+        >
           HOME
-        </a>
+        </Link>
       </div>
-      <div className={selectedMenu === "ROOM TYPE" ? "border-b-2" : ""}>
-        <a href="" className="text-white font-inter" onClick={() => handleMenuClick("ROOM TYPE")}>
+      <div
+        className={
+          selectedMenu === "ROOM TYPE" ? "border-b-2 border-yellow-600" : ""
+        }
+      >
+        <Link
+          to="/room-type"
+          className="text-white font-inter"
+          onClick={() => handleMenuClick("ROOM TYPE")}
+        >
           ROOM TYPE
-        </a>
+        </Link>
       </div>
-      <div className={selectedMenu === "ABOUT" ? "border-b-2" : ""}>
-        <a href="" className="text-white font-inter" onClick={() => handleMenuClick("ABOUT")}>
+      <div
+        className={
+          selectedMenu === "ABOUT" ? "border-b-2 border-yellow-600" : ""
+        }
+      >
+        <Link
+          to="/about"
+          className="text-white font-inter"
+          onClick={() => handleMenuClick("ABOUT")}
+        >
           ABOUT
-        </a>
+        </Link>
       </div>
-      <div className="ml-16">
-        <p className="text-white mx-96 text-5xl font-playfair">Midnight & Haven</p>
+      <div className="ml-auto mr-auto">
+        <p className="text-white mx-96 text-5xl font-playfair">
+          Midnight & Haven
+        </p>
       </div>
       <div className="mr-18">
-        <CiSearch color="white" size={40} />
+        <CiSearch color="white" size={40} onClick={handleSearchButtonClick} />
       </div>
+      {isVisible && (
+        <input
+          type="text"
+          className="border-2 border-gray-400 rounded-md h-10 w-96"
+        />
+      )}
       <div className="mr-6">
         <IoPersonCircleOutline color="white" size={40} />
       </div>
