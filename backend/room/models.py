255af26d716806f62fbcee82cd4b7f4a62191e7e
THIS ONE IS A LITTLE BIT TRICKY KRUB <pattadon2546pn@gmail.com>
2024-01-09 22:16:36 +0700

    Add RoomType and RoomDetail models

--- backend/room/models.py
+++ backend/room/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class RoomType(models.Model):
+    roomtype_name = models.CharField(max_length=50, unique=True, blank=False)
+
+
+class RoomDetail(models.Model):
+    roomtype_id = models.ForeignKey(RoomType)
+    room_num = models.IntegerField(unique=True, blank=False)
+    is_available = models.BooleanField(blank=False)
