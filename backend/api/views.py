a29d9d8329b9e056db2d6b9ac3c8fe349f9abc86
THIS ONE IS A LITTLE BIT TRICKY KRUB <pattadon2546pn@gmail.com>
2024-01-09 22:03:57 +0700

    Add permission classes to CustomerViewSet and create Reservation model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,10 +1,11 @@
 import sys
 sys.path.append("..")
-from django.shortcuts import render
 from .serializers import CustomerSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from customer.models import Customer
 from rest_framework import viewsets
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all().order_by('first_name')
     serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
