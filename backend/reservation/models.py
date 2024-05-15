38efc8a55ff6f3caf899f6a6ba7521bcaf22890b
THIS ONE IS A LITTLE BIT TRICKY KRUB <pattadon2546pn@gmail.com>
2024-05-15 17:27:25 +0700

    chore: Add Reservation model and update Room and Customer models

--- backend/reservation/models.py
+++ backend/reservation/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from customer.models import Customer
+from room.models import Room
+
 
 class Reservation(models.Model):
-    pass
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    room_id = models.ForeignKey(Room, on_delete=models.CASCADE)
+    check_in_date = models.DateTimeField()
+    check_out_date = models.DateTimeField()
--- backend/room/models.py
+++ backend/room/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 class RoomType(models.Model):
-    roomtype_name = models.CharField(max_length=50, unique=True, blank=False)
+    room_type_name = models.CharField(max_length=50, unique=True, blank=False)
 
 
-class RoomDetail(models.Model):
-    roomtype_id = models.ForeignKey(RoomType)
+class Room(models.Model):
+    room_type_id = models.ForeignKey(RoomType, on_delete=models.CASCADE)
     room_num = models.IntegerField(unique=True, blank=False)
     is_available = models.BooleanField(blank=False)
+    price = models.FloatField(blank=False)
